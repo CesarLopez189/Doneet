52f24db78220a3d3cdcf833d81a119428ee74826
Cesar Lopez <clopezd1501@alumno.ipn.mx>
2024-04-06 17:23:43 -0600

    Ruta estadisticas

---
+++ src/controllers/estadisticas.controller.js
@@ -0,0 +1,7 @@
+const estadisticasCtrl = {};
+
+estadisticasCtrl.renderEstadisticas = (req, res) => {
+    res.render('estadisticas')
+}
+
+module.exports = estadisticasCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,12 @@ const { Router } = require('express');
 const router = Router();
 
 const { renderIndex, renderAbout} = require('../controllers/index.controller')
+const { renderEstadisticas } = require('../controllers/estadisticas.controller');
 
 router.get('/', renderIndex);
 
 router.get('/about', renderAbout);
+
+router.get('/estadisticas', renderEstadisticas);
 
 module.exports = router;
