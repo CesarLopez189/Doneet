d0ca76d10c28e61f54f911372de2b4a35b1e0627
Cesar Lopez <clopezd1501@alumno.ipn.mx>
2024-05-26 01:06:01 -0600

    Pantalla usuario

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,10 +4,12 @@ const router = Router();
 const { renderIndex, renderAbout } = require('../controllers/index.controller');
 const { renderEstadisticas } = require('../controllers/estadisticas.controller');
 const { reportarAlergeno } = require('../controllers/reporteAlergenoController');
+const { updateUser } = require('../controllers/users.controller');
 
 router.get('/', renderIndex);
 router.get('/about', renderAbout);
 router.get('/estadisticas', renderEstadisticas);
 router.post('/reportar-alergeno', reportarAlergeno);
+router.post('/usuarios/update', updateUser);
 
 module.exports = router;
