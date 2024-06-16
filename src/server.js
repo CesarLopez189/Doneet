1e0414379f6dd5f6386feb7b88741a609ac3fe29
Cesar Lopez <clopezd1501@alumno.ipn.mx>
2024-06-15 21:01:19 -0600

    Cambios esteticos

--- src/server.js
+++ src/server.js
@@ -41,7 +41,32 @@ const hbs = exphbs.create({
                 }
             }
             return options.inverse(this);
-        }
+        },
+        ifCond: function (v1, operator, v2, options) {
+            switch (operator) {
+              case '==':
+                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+              case '===':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+              case '<':
+                return (v1 < v2) ? options.fn(this) : options.inverse(this);
+              case '<=':
+                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+              case '>':
+                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+              case '>=':
+                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+              case '&&':
+                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+              case '||':
+                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+              default:
+                return options.inverse(this);
+            }
+          },
+          json: function (context) {
+            return JSON.stringify(context);
+          },
     }
 });
 
